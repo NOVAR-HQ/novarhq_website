8400bfdf33baeaa297aa12e3eabaf0499cbc2721
NOVAR-HQ <novarhq.socials@gmail.com>
2025-03-13 23:12:52 +0100

    scroll distance 200

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -7,7 +7,7 @@ export default function ScrollIndicator() {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 200) {
         setIsVisible(false);
       }
     };
